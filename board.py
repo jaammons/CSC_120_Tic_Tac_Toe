b1a2c7122185e7f836da4cb620384dc4858fa513
James Ammons <jaammons@my.waketech.edu>
2022-07-17 14:04:12 -0400

    Functionality

--- board.py
+++ board.py
@@ -1,1 +1,16 @@
-print("Hello World")
+gameboard = ["-", "-", "-",
+             "-", "-", "-",
+             "-", "-", "-"]
+player = "X"
+gameCurrent = True
+win = None
+
+def printboard(gameboard):
+    print(gameboard[0] + " | " + gameboard[1] + " | " + gameboard[2])
+    print("")
+    print(gameboard[3] + " | " + gameboard[4] + " | " + gameboard[5])
+    print("")
+    print(gameboard[6] + " | " + gameboard[7] + " | " + gameboard[8])
+printboard(gameboard)
+
+
